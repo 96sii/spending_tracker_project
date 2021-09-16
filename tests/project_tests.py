1423edb0a63cb2fa5c22f41b109192f9f607a58e
Simon Fraser <sii_fraser@hotmail.co.uk>
2021-09-16 10:23:23 +0100

    Added testing for all classes

--- tests/project_tests.py
+++ tests/project_tests.py
@@ -11,3 +11,9 @@ class TestProject(unittest.TestCase):
 
     def test_merchant(self):
         self.assertEqual('Tesco', self.merchant.name)
+
+    def test_category(self):
+        self.assertEqual('Groceries', self.category.category)
+
+    def test_transaction(self):
+        self. assertEqual('2021-09-14', self.transaction.date)
