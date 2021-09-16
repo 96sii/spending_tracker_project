ca18cfeb28698e352bdbeafa839920cb31c1e175
Simon Fraser <sii_fraser@hotmail.co.uk>
2021-09-16 10:42:53 +0100

    Imported Flask into controller files. Website runs successfully

---
+++ controllers/merchants_controller.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+
+
+
+
+merchants_blueprint = Blueprint("merchants", __name__)
---
+++ controllers/transactions_controller.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+
+
+
+
+transactions_blueprint = Blueprint("transactions", __name__)
