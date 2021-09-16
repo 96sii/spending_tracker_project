799178c1ecfb50704bbb8a1f1688123b5709b7ab
Simon Fraser <sii_fraser@hotmail.co.uk>
2021-09-16 17:10:41 +0100

    Created transactions/new.html and created form

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 import repositories.transaction_repositories as transaction_repository
 import repositories.merchant_repository as merchant_repository
+import repositories.category_repository as category_repository
 
 
 
@@ -13,3 +14,9 @@ def transactions():
     transactions = transaction_repository.select_all()
     total = transaction_repository.add_total()
     return render_template("/transactions/index.html", transactions=transactions, total=total)
+
+@transactions_blueprint.route("/transactions/new", methods=['GET'])
+def new_transaction():
+    categories = category_repository.select_all()
+    merchants = merchant_repository.select_all()
+    return render_template("transactions/new.html", categories=categories, merchants=merchants)
